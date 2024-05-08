0d8a1dcfb97a09fc8a854c0c52762a1904edb38b
ysnbhb <ybahbib@b1f1r9s9>
2024-05-08 16:52:04 +0100

    ascii art

--- ascii-art-output/split.go
+++ ascii-art-output/split.go
@@ -7,7 +7,7 @@ import (
 func Check(s string) {
 	for _, i := range s {
 		if i > 126 || i < 32 {
-			log.Fatal("there are invalide agre in text ")
+			log.Fatal("you use invalide alpha in text")
 		}
 	}
 }

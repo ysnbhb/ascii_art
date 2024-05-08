cf8c8a88c7a01553fc3a742bc3615a1ce2d292fe
ysnbhb <ybahbib@b1f1r9s9>
2024-05-08 11:37:01 +0100

    output

--- ascii-art-output/chach.go
+++ ascii-art-output/chach.go
@@ -3,52 +3,42 @@ package art
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"regexp"
 )
 
 func Checke() {
 	arr := os.Args[1:]
 	if len(arr) > 3 || len(arr) == 0 {
-		fmt.Println("ok")
+		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 		return
 	}
 	_thre, numbretrue := Is_out(arr[0])
 	if len(arr) == 1 {
 		if (_thre && numbretrue == 0) || !_thre {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 			return
 		}
 		if _thre {
-			cmd := exec.Command("go", "run", "../main/main.go", arr[0])
-			out, _ := cmd.Output()
-			fmt.Print(string(out))
+			Func1(arr[0], "", "standard")
 			return
 		}
 
 	} else if len(arr) == 2 {
 		if !_thre {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 			return
 		}
 		if _thre && numbretrue == 0 {
-			file, _ := os.Create(arr[0][9:])
-			cmd := exec.Command("go", "run", "../main/main.go", arr[1])
-			out, _ := cmd.Output()
-			file.Write(out)
+			Func1(arr[1], arr[0][9:], "standard")
 			return
 		}
-		cmd := exec.Command("go", "run", "../main/main.go", arr[0], arr[1])
-		out, _ := cmd.Output()
-		fmt.Print(string(out))
+		Func1(arr[0], "", arr[1])
 	} else if len(arr) == 3 {
 		if _thre && numbretrue == 0 {
-			cmd := exec.Command("go", "run", "../main/main.go", arr[1], arr[2])
-			out, _ := cmd.Output()
-			if string(out) == "Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard\n" {
-				return
-			}
-			file, _ := os.Create(arr[0][9:])
-			file.Write(out)
+			Func1(arr[1], arr[0][9:], arr[2])
 			return
 		} else {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 			return
 		}
 	}
@@ -63,4 +53,21 @@ func Is_out(s string) (bool, int) {
 		}
 	}
 	return true, 1
+}
+
+func Func1(s, filecreate, file string) {
+	if s == "" {
+		return
+	}
+	if s == `\n` {
+		s = ""
+	}
+	mapDraw, err := Art(file)
+	if err != nil {
+		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
+		return
+	}
+	re := regexp.MustCompile(`\A((\\n)+)\\n$`)
+	s = re.ReplaceAllString(s, "$1")
+	SplitAndPrint(s, filecreate, mapDraw)
 }
--- ascii-art-output/printArt.go
+++ ascii-art-output/printArt.go
@@ -2,7 +2,26 @@ package art
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"time"
 )
+
+func SplitAndPrint(s, file string, mapDraw map[int][]string) {
+	split_backN := strings.Split(s, `\n`)
+	var stoc string
+	if file == "" {
+		for _, i := range split_backN {
+			PrintArt(i, mapDraw)
+		}
+	} else {
+		for _, i := range split_backN {
+			stoc += Stoc(i, mapDraw)
+		}
+		filstoc, _ := os.Create(file)
+		filstoc.WriteString(stoc)
+	}
+}
 
 func PrintArt(s string, a map[int][]string) {
 	if s == "" {
@@ -15,10 +34,30 @@ func PrintArt(s string, a map[int][]string) {
 		}
 		for i := 0; i < 8; i++ {
 			for j = 0; j < len(s); j++ {
-				//	time.Sleep(time.Duration((370000000 / len(s))))
+				time.Sleep(time.Duration((370000000 / len(s))))
 				fmt.Print(asciiArt[j][i])
 			}
 			fmt.Println()
 		}
 	}
+}
+
+func Stoc(s string, a map[int][]string) string {
+	var returnsrt string
+	if s == "" {
+		returnsrt += "\n"
+	} else {
+		var asciiArt [][]string
+		j := 0
+		for _, p := range s {
+			asciiArt = append(asciiArt, a[int(p)])
+		}
+		for i := 0; i < 8; i++ {
+			for j = 0; j < len(s); j++ {
+				returnsrt += asciiArt[j][i]
+			}
+			returnsrt += "\n"
+		}
+	}
+	return returnsrt
 }
